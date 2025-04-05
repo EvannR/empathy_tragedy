62e73becaed9a71f9230e0d10631b0b640911f43
jeromeis <jeromeiste@gmail.com>
2025-04-05 17:21:06 +0200

    idea about the visualization

---
+++ game_theoretic_setup/game_theoretic_vizualization.py
@@ -0,0 +1,12 @@
+"""
+Idea :
+Agents = circles + table indicating the global amount per agent
+    choice of eating = +1 displayed on the screen
+    choice of not eating = +0 displayed on the screen
+
+    emotions = color of the agent (red: bad // green: good)
+
+A colum indicates the number of ressources available
+
+
+"""
